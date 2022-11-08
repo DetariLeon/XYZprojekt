680b0b17bc402bd3762c4f46e42954ff809a6670
David95406 <83376469+David95406@users.noreply.github.com>
2022-11-08 12:47:00 +0100

    2. feladat
    Y

--- DDL/src/ddl/DDL.java
+++ DDL/src/ddl/DDL.java
@@ -1,7 +1,11 @@
 package ddl;
 
 public class DDL {
-
+    
+    public static double hengerTerfogat(double r, double m) {
+        return (Math.pow(r, 2) * Math.PI * m);
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
