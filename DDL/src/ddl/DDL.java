91c65054b7b119e7be098bcd7ca245c990e44a39
Domek1232 <83282741+Domek1232@users.noreply.github.com>
2022-11-08 12:52:42 +0100

    3.Feladat Z

--- DDL/src/ddl/DDL.java
+++ DDL/src/ddl/DDL.java
@@ -1,13 +1,17 @@
 package ddl;
 
 public class DDL {
-    
+
     public static double hengerTerfogat(double r, double m) {
         return (Math.pow(r, 2) * Math.PI * m);
     }
-    
+
+    public static double hengerFelszin(double r, double m) {
+        return (2 * Math.pow(r, 2) * Math.PI + 2 * r * Math.PI * m);
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
